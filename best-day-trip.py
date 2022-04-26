49dfeb8b6737fa7361c7378ff8eb1e552abdb33f
babyslikk <brianw.blake@yahoo.com>
2022-04-25 20:12:06 -0500

    created destination, food, transportation, entertainment list

---
+++ best-day-trip.py
@@ -0,0 +1,9 @@
+import random
+
+dest = ['Washington Dc', 'Santa Monica', 'Las Vegas', 'New York', 'France']
+food = ['Weber Grille', 'The Capital Grille', 'In n out burger', 'Texas Longhorn', 'Burnt Toast']
+trans = ['Jet', 'Car', 'Cruise Ship', 'Airplane', 'Amtrak']
+ent = ['Sporting Event', 'Movies', 'Theme Park', ' Concerts', 'Theater']
+
+welcome_message = 'If you want the best Day Trip planner, you came to the right place!'
+print(welcome_message)
